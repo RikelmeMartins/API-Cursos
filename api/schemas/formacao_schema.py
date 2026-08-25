65733c0344aafabd3ffaadb7de944a51af2b4389
RikelmeMartins <rikelme2005@gmail.com>
2026-08-25 19:34:07 -0300

    Listando os cursos de cada formação

--- api/schemas/formacao_schema.py
+++ api/schemas/formacao_schema.py
@@ -1,13 +1,14 @@
 from api import ma
 from ..models import formacao_model
 from marshmallow import fields
+from ..schemas import curso_schema
 
 class FormacaoSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = formacao_model.Formacao
         _load_instance = True
-        _declared_fields = ("id", "nome", "descricao")
+        _declared_fields = ("id", "nome", "descricao", "cursos")
 
     nome = fields.String(required=True)
     descricao = fields.String(required=True)
-    
+    cursos = fields.List(fields.Nested(curso_schema.CursoSchema, only=('id', 'nome')))
