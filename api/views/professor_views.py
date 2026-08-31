ff58c137b460c2c973b5e1d0101b18a5c3352498
RikelmeMartins <rikelme2005@gmail.com>
2026-08-31 13:36:37 -0300

    implementação do paginate na api

--- api/views/professor_views.py
+++ api/views/professor_views.py
@@ -4,12 +4,12 @@ from ..schemas import professor_schema
 from flask import request, make_response, jsonify
 from ..entidades import professor
 from ..services import professor_service
-
+from ..paginate import paginate
+from ..models.professor_model import Professor
 class ProfessorList(Resource):
     def get(self):
-        professores = professor_service.listar_professores()
         pr = professor_schema.ProfessorSchema(many=True)
-        return make_response(pr.jsonify(professores), 200)
+        return paginate(Professor, pr)
     
     def post(self):
         pr = professor_schema.ProfessorSchema()
